737eaf96c7c80d0f98180e564f3753684efebb5b
tomeklarysz <ttomek.larysz@gmail.com>
2025-05-31 15:40:20 +0200

    formatted files

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,26 +1,26 @@
 let users = [
-    {
-        id: 1,
-        name: "lpc-10"
-    },
-    {
-        id: 2,
-        name: "bober kurwa"
-    }
+  {
+    id: 1,
+    name: "lpc-10"
+  },
+  {
+    id: 2,
+    name: "bober kurwa"
+  }
 ];
 
 exports.getUsers = (req, res) => {
-    res.json(users)
+  res.json(users)
 };
 
 exports.createUser = (req, res) => {
-    const { name } = req.body;
-    const newUser = {
-        id: users.length + 1,
-        name
-    }
-    users.push(newUser);
-    res.status(201).json(newUser);
+  const { name } = req.body;
+  const newUser = {
+    id: users.length + 1,
+    name
+  }
+  users.push(newUser);
+  res.status(201).json(newUser);
 }
 
 // createUser TODO:
@@ -30,13 +30,13 @@ exports.createUser = (req, res) => {
 // generowanie unikalnego id
 
 exports.deleteUser = (req, res) => {
-    const { id } = req.params;
-    const userIndex = users.findIndex(user => user.id === parseInt(id));
-    
-    if (userIndex === -1) {
-        return res.status(404).json({ message: 'User not found' });
-    }
-    
-    users.splice(userIndex, 1);
-    res.status(204).send();
+  const { id } = req.params;
+  const userIndex = users.findIndex(user => user.id === parseInt(id));
+
+  if (userIndex === -1) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  users.splice(userIndex, 1);
+  res.status(204).send();
 }
