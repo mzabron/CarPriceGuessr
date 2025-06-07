bb64c4ae45668e945edb7b0ea5cf9d7e26371580
tomeklarysz <ttomek.larysz@gmail.com>
2025-06-08 00:53:41 +0200

    fetch cars after starting game attempt

--- frontend/src/components/GameContent.js
+++ frontend/src/components/GameContent.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import socketService from '../services/socketService';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import apiService from '../services/apiService';
+import { useNavigate } from 'react-router-dom';
 
 const GameContent = () => {
+  const { navigate } = useNavigate();
+  const [carJson, setCarJson] = useState(null);
+  // const [carImage, setCarImage] = useState(null);
+
+  useEffect(() => {
+    socketService.connect();
+    socketService.socket?.on('game:start', fetchCars);
+  },);
+
+  const fetchCars = async () => {
+      const fetchedCars = await apiService.getCars();
+      console.log(fetchedCars);
+      setCarJson = fetchCars
+        // setCarJson = await fetchedCars.json();
+    };
+
   return (
     <div className="flex-1 bg-white p-4">
       <div className="h-full flex flex-col">
         <h1 className="text-3xl font-bold mb-4">Game in Progress</h1>
         <div className="flex-1 bg-gray-100 rounded-lg p-4">
-          Game content coming soon...
+          {/* Game content coming soon...
+           */
+           carJson}
         </div>
       </div>
     </div>
