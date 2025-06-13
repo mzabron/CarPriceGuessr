d7b941404501727c06f532d9afac5f9351802d51
Karol <karolarabasz6@gmail.com>
2025-06-13 19:06:56 +0200

    user login and logout

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,20 @@
+// const express = require('express');
+// const router = express.Router();
+// const { getUsers, createUser, deleteUser } = require('../controllers/userController');
+
+// router.get('/', getUsers);
+
+// router.post('/', createUser);
+
+// router.delete('/:id', deleteUser);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
-const { getUsers, createUser, deleteUser } = require('../controllers/userController');
-
-router.get('/', getUsers);
+const { createUser, loginUser } = require('../controllers/userController');
 
 router.post('/', createUser);
-
-router.delete('/:id', deleteUser);
+router.post('/login', loginUser);
 
 module.exports = router;
