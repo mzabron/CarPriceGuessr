6140f37d72bedf29759ba77060d743b41b3b8800
tomeklarysz <ttomek.larysz@gmail.com>
2025-06-29 15:35:26 +0200

    add frontend url on backend

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const connectDB = require('./db');
 const app = express();
 const server = createServer(app);
 
-const URL = 'http://localhost:3000';
+const URL = 'https://tlarysz.lab.kis.agh.edu.pl/';
 const io = new Server(server, {
   path: '/ws',
   cors: {
